57597b4c7e28d08ba46d248d136a28f1572e9099
M17B30 <agrawalbhavish07@gmail.com>
2025-09-01 22:19:33 +0530

    updates

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,7 +1,1 @@
-// window.onscroll = function() {onscrollfunction()};
-// function onscrollfunction() {
-//     if(document.documentElement.scrollTop > 350){
-//         document.getElementById('name').className = 'fadein'
-//     }
-// }
 "use strict";
--- main.js
+++
@@ -1,7 +0,0 @@
-// window.onscroll = function() {onscrollfunction()};
-
-// function onscrollfunction() {
-//     if(document.documentElement.scrollTop > 350){
-//         document.getElementById('name').className = 'fadein'
-//     }
-// }

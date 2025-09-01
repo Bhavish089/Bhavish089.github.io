52e321d7f0967360e6ca588420c8c7e07d695b8d
M17B30 <agrawalbhavish07@gmail.com>
2025-09-01 09:35:08 +0530

    updates

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,7 +1,7 @@
 // window.onscroll = function() {onscrollfunction()};
-
 // function onscrollfunction() {
 //     if(document.documentElement.scrollTop > 350){
 //         document.getElementById('name').className = 'fadein'
 //     }
 // }
+"use strict";
